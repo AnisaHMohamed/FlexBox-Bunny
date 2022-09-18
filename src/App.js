50801754b2d46fc0236ea909e6e70d8351030936
AnisaHMohamed <anisahamohamed@gmail.com>
2022-09-18 13:58:41 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import logo from './bunny.png';
+import tree from './lemon_tree.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="pulse" alt="bunny" />
+        <img  src={tree} className="pulse" alt="lemon tree"></img>
+      
       </header>
     </div>
   );
