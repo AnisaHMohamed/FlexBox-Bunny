1661e774f041deab2d0994cecd290e93a0295f50
AnisaHMohamed <anisahamohamed@gmail.com>
2022-09-18 15:41:44 -0400

    add level one

--- src/components/GameContainer/index.js
+++ src/components/GameContainer/index.js
@@ -1,11 +1,16 @@
 import "./style.css";
 import bunny from "../../assets/bunny.png"
-import lemon_tree from "../../assets/lemon_tree.png"
+// import lemon_tree from "../../assets/lemon_tree.png"
+// <img  src={lemon_tree} alt="lemon tree" ></img>  
 const GameContainer = () => {
   return (
     <div className="Game-container">
+    <div className="board">
     <img src={bunny} className="pulse" alt="bunny" />
-    <img  src={lemon_tree} alt="lemon tree"></img>    </div>
+
+    </div>
+ 
+      </div>
   );
 }
 
--- src/components/Level/index.js
+++ src/components/Level/index.js
@@ -3,10 +3,10 @@ import "./style.css";
 const Level = ({level}) => {
    console.log(level)
   return (
-    <>
+    <div>
    
   <p>level: {1}</p>
- <h1>Welcome to Flexbox Froggy, a game where you help Froggy and friends by writing CSS code! Guide this frog to the lilypad on the right by using the justify-content property, which aligns items horizontally and accepts the following values:
+ <h1>Welcome to Flexbox Bunny, a game where you help Bunny and friends by writing CSS code! Guide this bunny to the tree on the right by using the justify-content property, which aligns items horizontally and accepts the following values:
  </h1> 
  <h2>
 flex-start: Items align to the left side of the container.
@@ -14,9 +14,9 @@ flex-end: Items align to the right side of the container.
 center: Items align at the center of the container.
 space-between: Items display with equal spacing between them.
 space-around: Items display with equal spacing around them.
-For example, justify-content: flex-end; will move the frog to the right. 
+For example, justify-content: flex-end; will move the bunny to the right. 
 </h2>
-</>
+</div>
   );
 }
 
